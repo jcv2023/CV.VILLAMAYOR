a5c5b71be1d0898a92dcd6b88b958d763fd9d24a
jcv2023 <123260211+jcv2023@users.noreply.github.com>
2023-01-22 02:21:32 -0300

    Add files via upload

---
+++ js/funcion.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function () {
+	console.log("Capturamos el evento click");
+	document.getElementById("demo").innerHTML = ("Introducción a BIG DATA - Fundación Telefónica Movistar Argentina - Expedición: Ene. 2023 - Sin fecha de vencimiento - ID de la credencial e64e4fc6-48d0-4092-b5b7-4792ef055937");
+	}
